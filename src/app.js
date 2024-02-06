53ff1d80b8197b95c59bbe7fc79db5423e610735
stanxlin <143833064+stanxlin@users.noreply.github.com>
2024-02-06 01:18:07 +0000

    js file to run random doman names

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//write your code here
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extensions = [".com", ".net", ".us", ".io", ".org"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      console.log(
+        pronoun[i] +
+          adj[j] +
+          noun[k] +
+          extensions[Math.floor(Math.random() * extensions.length)]
+      );
+    }
+  }
+}
